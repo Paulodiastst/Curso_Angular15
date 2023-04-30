60cb7f2e1580f07ca72e1003809cd80da0e7f0d9
Paulo Roberto <paulodiastst@hotmail.com>
2023-04-30 15:59:17 -0300

    Diretivas de decisão *ngIf e ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,25 +7,13 @@ import {Component} from "@angular/core";
 })
 export class AppComponent {
 
-  title = 'Angular';
-
-  imgUrl = "https://kinsta.com/pt/wp-content/uploads/sites/3/2022/07/angular-vs-vue-1024x512.jpg";
+  title = 'Olá';
 
   constructor() {
   }
 
-  chamarFuncao(){
-    console.log("Esse é um click")
-  }
+  // ngIf usando complementos else, usamos para exiber ou ocultar elementos tomada de decisão logica.
 
-  //<---- 4 tipo de data Binding ---->
-
-  // Interpretação {{title}}
-
-  // Property bind <img [src]="imgUrl">
-
-  // Eventy binding <button (click)="chamarFuncao()">
-
-  // Two-way databinding < input [(ngModel)]="title">
+  // ngSwitch
 
 }
